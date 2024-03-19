523a6332b740763dce07e2dd86f5eed8aa89f968
Samuel Seguins <samuelseguins@gmail.com>
2024-03-19 15:37:38 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const http = require('http');
+const socketIO = require('socket.io');
+// const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIO(server)
+
+//liberar o uso livre da api
+app.use(cors());
+
+app.get('/', (req, res) => {
+    const socketAddress = `http://${req.hostname}:${server.address().port}`;  
+    res.json({ socketAddress });
+});
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log(`Servidor HTTP e WebSocket escutando na porta ${PORT}`);
+});
+
+// Configurações adicionais do Socket.IO
+io.on('connection', (socket) => {
+    console.log('Um cliente se conectou');
+
+    socket.on('disconnect', () => {
+        console.log('Um cliente se desconectou');
+    });
+  // Manipule outros eventos do Socket.IO conforme necessário
+});
+
