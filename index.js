14d5b434e76da1e5764dd9e1585be722fce4acc6
Samuel Seguins <samuelseguins@gmail.com>
2024-03-20 00:42:58 -0300

    mudado metodos de contrução do server

--- index.js
+++ index.js
@@ -1,26 +1,31 @@
-const express = require('express');
+// const express = require('express');
 const http = require('http');
 const socketIO = require('socket.io');
-// const bodyParser = require('body-parser');
-const cors = require('cors');
-const { hostname } = require('os');
 
-const app = express();
-const server = http.createServer(app);
-const io = socketIO(server)
+// const bodyParser = require('body-parser');
+// const cors = require('cors');
+// const { hostname } = require('os');
+
+// const app = express();
+// const server = http.createServer(app);
+// const io = socketIO(server)
+
+const httpServer = http.createServer();
+const io = socketIO(httpServer, {
+  cors: {
+    origin: "https://client-side-production.up.railway.app/",
+    credentials : true
+  }
+})
+
 
 //liberar o uso livre da api
-app.use(cors());
+// app.use(cors());
 
-app.get('/config', (req, res) => {
-    const socketAddress = `wss://${req.hostname}:${server.address().port}/`;  
-    res.json({ socketAddress });
-});
-
-const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => {
-  console.log(`Servidor HTTP e WebSocket escutando na porta ${PORT}`);
-});
+// app.get('/config', (req, res) => {
+//     const socketAddress = `wss://${req.hostname}:${server.address().port}/`;  
+//     res.json({ socketAddress });
+// });
 
 // Configurações adicionais do Socket.IO
 io.on('connection', (socket) => {
@@ -32,3 +37,8 @@ io.on('connection', (socket) => {
   // Manipule outros eventos do Socket.IO conforme necessário
 });
 
+const PORT = process.env.PORT || 3000;
+
+httpServer.listen(PORT, () => {
+  console.log(`Servidor HTTP e WebSocket escutando na porta ${PORT}`);
+});
