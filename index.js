72b5639706b33935b4b73694ba4063aa66fd35f6
Samuel Seguins <samuelseguins@gmail.com>
2024-03-20 23:45:40 -0300

    adicionado server express com distribuição da pagina master

--- index.js
+++ index.js
@@ -1,44 +1,38 @@
-// const express = require('express');
 const http = require('http');
 const socketIO = require('socket.io');
 
-// const bodyParser = require('body-parser');
-// const cors = require('cors');
-// const { hostname } = require('os');
+const express = require('express');
+const app = express();
 
-// const app = express();
-// const server = http.createServer(app);
-// const io = socketIO(server)
+const httpServer = http.createServer(app);
+const PORT = process.env.PORT || 3000;
 
-const httpServer = http.createServer();
-const io = socketIO(httpServer, {
-  cors: {
-    //https://client-side-production.up.railway.app/
-    origin: 'https://client-side-production.up.railway.app',
-    credentials : true
-  }
+app.get('/page-master', (req,res) => {
+  res.sendFile(__dirname + '/public/index.html');
 })
 
 
-//liberar o uso livre da api
-// app.use(cors());
-
-// app.get('/config', (req, res) => {
-//     const socketAddress = `wss://${req.hostname}:${server.address().port}/`;  
-//     res.json({ socketAddress });
-// });
+const io = socketIO(httpServer, {
+  cors: {
+    //https://client-side-production.up.railway.app
+    origin: 'https://client-side-production.up.railway.app' , //`http://localhost:${PORT}`],
+    credentials : true
+  }
+})
 
 // Configurações adicionais do Socket.IO
 io.on('connection', (socket) => {
     console.log('Um cliente se conectou');
 
+    socket.on('message', (message) => {
+      console.log('mensage vinda de um client', message);
+      io.emit('message', message);
+    })
+
     socket.on('disconnect', () => {
         console.log('Um cliente se desconectou');
     });
-  // Manipule outros eventos do Socket.IO conforme necessário
 });
-
-const PORT = process.env.PORT || 3000;
 
 httpServer.listen(PORT, () => {
   console.log(`Servidor HTTP e WebSocket escutando na porta ${PORT}`);
